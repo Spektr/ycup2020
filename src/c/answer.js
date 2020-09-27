6d103933f7cd7199fb3d077d4a80378dc56b934f
Alexander <spektrasm@gmail.com>
2020-09-27 14:33:28 +0300

    add conditions

---
+++ src/c/answer.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @typedef CoffeeMachineDebugInfo
+ * @property {string} id
+ * @property {number} code
+ * @property {string} message
+ */
+
+/**
+ * Отрисовать отладочную информацию кофемашины в element
+ * @param debugInfo {CoffeeMachineDebugInfo} - отладочная информация
+ * @param element {HTMLDivElement} - div с фиксированным размером 300x96 пикселей,
+ *     в который будет отрисовываться баркод
+ */
+function renderBarcode(debugInfo, element) {
+    // ваш код
+}
